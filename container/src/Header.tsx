eabe7ec853292c637ffb7f606dcd1db31a24ab4a
jsg3121 <xodm95gmail.com>
2022-06-09 17:52:55 +0900

    FEAT(header) : header hover효과 추가 및 아이콘 추가

--- container/src/Header.tsx
+++ container/src/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import isEqual from 'fast-deep-equal'
 import header from 'styles/header.module.scss'
+import Image from 'next/image'
 
 const Header: React.FC = () => {
   return (
@@ -8,18 +9,51 @@ const Header: React.FC = () => {
       <div className={header.header}>
         <ul className={header.header__navigation}>
           <li>
-            <h1>About Me</h1>
+            <h1 data-content="About Me">About Me</h1>
           </li>
           <li>
-            <h1>Skills</h1>
+            <h1 data-content="Skills">Skills</h1>
           </li>
           <li>
-            <h1>Project</h1>
+            <h1 data-content="Project">Project</h1>
           </li>
           <li>
-            <h1>Contact</h1>
+            <h1 data-content="Content">Contact</h1>
           </li>
         </ul>
+        <div className={header.header__logo}>
+          <ul className={header['header__logo-list']}>
+            <li>
+              <picture>
+                <figure className={header['header__logo--git-motion']}>
+                  <Image
+                    src="/github.svg"
+                    alt="github"
+                    layout="fill"
+                    priority
+                  />
+                </figure>
+              </picture>
+            </li>
+            <li>
+              <picture>
+                <figure>
+                  <Image src="/velog.svg" alt="velog" layout="fill" priority />
+                </figure>
+              </picture>
+            </li>
+            <li>
+              <picture>
+                <figure></figure>
+              </picture>
+            </li>
+            <li>
+              <picture>
+                <figure></figure>
+              </picture>
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   )
