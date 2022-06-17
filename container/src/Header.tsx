b6064c03cd42f9e78298b9630a171f16984b3a2f
jsg3121 <xodm95@gmail.com>
2022-06-17 17:49:04 +0900

    #1 svg수정 및 토글 추가

--- container/src/Header.tsx
+++ container/src/Header.tsx
@@ -2,18 +2,50 @@ import { HeaderNav, HeaderSNB, Image } from 'components'
 import isEqual from 'fast-deep-equal'
 import React from 'react'
 import header from 'styles/header.module.scss'
+import { gsap } from 'gsap'
 
 const Header: React.FC = () => {
   const [isShow, setIsShow] = React.useState<boolean>(false)
+  const [isHide, setIsHide] = React.useState<boolean>(true)
+
+  const headerRef = React.useRef<HTMLDivElement>(null)
+  const headerButtonRef = React.useRef<HTMLButtonElement>(null)
+
+  const handleClickHeader = React.useCallback(() => {
+    gsap
+      .timeline()
+      .to(headerRef.current, {
+        right: isHide ? '0' : '-50%',
+        duration: 0.4,
+        ease: 'power3.out',
+      })
+      .to(
+        document.querySelectorAll(`.${header['header__nav-button']} picture`),
+        {
+          x: isHide ? '-3rem' : '0',
+          duration: 0.4,
+        },
+        -1
+      )
+
+    setIsHide(!isHide)
+  }, [isHide])
 
   return (
-    <div className={header.header}>
-      <button className={header['header__nav-button']}>
+    <>
+      <button
+        className={header['header__nav-button']}
+        ref={headerButtonRef}
+        onClick={handleClickHeader}
+      >
+        <Image src="/hamburger.svg" layout="fill" alt="nav_icon" priority />
         <Image src="/close.svg" layout="fill" alt="nav_icon" priority />
       </button>
-      <HeaderNav />
-      <HeaderSNB />
-    </div>
+      <div className={header.header} ref={headerRef}>
+        <HeaderNav />
+        <HeaderSNB />
+      </div>
+    </>
   )
 }
 
