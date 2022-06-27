1bad3929dd7cc50943093f1ff71ec559707bc5bf
jsg3121 <xodm95@gmail.com>
2022-06-27 18:50:20 +0900

    #17 ui , skill scroll issue 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
-import { Html } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { EffectComposer, SSAO } from '@react-three/postprocessing'
 import { Bubbles } from 'components'
-import { AboutMe, Introduce, ScrollController, Skills } from 'container'
+import { AboutMe, Introduce, Project, Skills } from 'container'
 import { gsap } from 'gsap'
 import ScrollToPlugin from 'gsap/dist/ScrollToPlugin'
 import ScrollTrigger from 'gsap/dist/ScrollTrigger'
@@ -25,34 +24,53 @@ const Home: NextPage = () => {
     }
   }, [])
 
+  React.useEffect(() => {
+    gsap.to('.horizontal-scroll', {
+      xPercent: -100,
+      x: () => window.innerWidth,
+      ease: 'none',
+      scrollTrigger: {
+        trigger: '.horizontal-scroll',
+        start: 'top top',
+        end: () => window.innerWidth * 2,
+        scrub: 1,
+        pin: true,
+        invalidateOnRefresh: true,
+      },
+    })
+  }, [])
+
   return (
-    <div className={background.container}>
-      <Canvas
-        shadows
-        dpr={[1, 2]}
-        gl={{ antialias: true }}
-        camera={{ fov: 50, position: [0, 0, 90], near: 20, far: 0 }}
-      >
-        <Bubbles />
-        <EffectComposer multisampling={0}>
-          <SSAO
-            samples={31}
-            radius={0.1}
-            intensity={20}
-            luminanceInfluence={0.1}
-            color="blue"
-          />
-        </EffectComposer>
-        <Html>
-          <Introduce />
+    <>
+      <div className={background.container}>
+        <Canvas
+          shadows
+          dpr={[1, 2]}
+          gl={{ antialias: true }}
+          camera={{ fov: 50, position: [0, 0, 90], near: 20, far: 0 }}
+          className={background.background}
+        >
+          <Bubbles />
+          <EffectComposer multisampling={0}>
+            <SSAO
+              samples={31}
+              radius={0.1}
+              intensity={20}
+              luminanceInfluence={0.1}
+              color="blue"
+            />
+          </EffectComposer>
+        </Canvas>
+      </div>
+      <Introduce />
+      <div className={`${background.horizontal} horizontal-scroll`}>
+        <div>
           <AboutMe />
           <Skills />
-        </Html>
-      </Canvas>
-      {/*
-        <Contact />
-        <Footer /> */}
-    </div>
+        </div>
+      </div>
+      <Project />
+    </>
   )
 }
 
