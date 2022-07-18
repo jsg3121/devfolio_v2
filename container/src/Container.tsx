2f7daea33a5b8f8a0124c20d06102e77846d2a72
jsg3121 <xodm95@gmail.com>
2022-07-18 10:52:13 +0900

    #50 속도 조정

--- container/src/Container.tsx
+++ container/src/Container.tsx
@@ -6,11 +6,11 @@ import React from 'react'
 import Scrollbar from 'smooth-scrollbar'
 import background from 'styles/background.module.scss'
 import AboutMe from './AboutMe'
+import Career from './Career'
 import Contact from './Contact'
 import Footer from './Footer'
 import Introduce from './Introduce'
 import Project from './Project'
-import Career from './Career'
 import WhoAmI from './WhoAmI'
 
 gsap.registerPlugin(TextPlugin, ScrollTrigger)
@@ -22,6 +22,7 @@ const Container: React.FC = () => {
 
     const scrollbar = Scrollbar.init(content, {
       damping: 0.05,
+      thumbMinSize: 1,
     })
     scrollbar.track.yAxis.element.remove()
 
